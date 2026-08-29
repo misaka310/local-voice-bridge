2f222944f75b436d550d52780022b17568e6c3a7
misaka310 <136806419+misaka310@users.noreply.github.com>
2026-08-30 06:17:11 +0900

    feat: mirror Local API runtime settings into browser

--- extension/background-settings-core.js
+++ extension/background-settings-core.js
@@ -145,13 +145,25 @@
       micConversationEnabled: Object.prototype.hasOwnProperty.call(remote, 'micConversationEnabled')
         ? Boolean(remote.micConversationEnabled)
         : Boolean(current.micConversationEnabled),
+      sttModel: Object.prototype.hasOwnProperty.call(remote, 'sttModel')
+        ? normalizeSttModel(remote.sttModel)
+        : normalizeSttModel(current.sttModel),
+      cancelGraceMs: Object.prototype.hasOwnProperty.call(remote, 'cancelGraceMs')
+        ? normalizeCancelGraceMs(remote.cancelGraceMs)
+        : normalizeCancelGraceMs(current.cancelGraceMs),
+      liveTtsProfile: Object.prototype.hasOwnProperty.call(remote, 'liveTtsProfile')
+        ? normalizeLiveTtsProfile(remote.liveTtsProfile)
+        : normalizeLiveTtsProfile(current.liveTtsProfile),
     };
     const changedReference = normalizeStoredReference(current.referenceVoice) !== referenceVoice;
     const changed = Boolean(current.enabled) !== next.enabled
       || Number(current.voiceVolume) !== next.voiceVolume
       || changedReference
       || normalizeStoredReference(current.voiceId) !== referenceVoice
-      || Boolean(current.micConversationEnabled) !== next.micConversationEnabled;
+      || Boolean(current.micConversationEnabled) !== next.micConversationEnabled
+      || normalizeSttModel(current.sttModel) !== next.sttModel
+      || normalizeCancelGraceMs(current.cancelGraceMs) !== next.cancelGraceMs
+      || normalizeLiveTtsProfile(current.liveTtsProfile) !== next.liveTtsProfile;
     return {
       changed,
       changedReference,
