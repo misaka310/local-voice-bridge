9f3cdf3d5eb28de1af3d1398c5d8bd72c70e36cd
misaka310 <136806419+misaka310@users.noreply.github.com>
2026-08-30 06:23:44 +0900

    test: include repair wiring regression

--- scripts/run-python-tests.js
+++ scripts/run-python-tests.js
@@ -11,6 +11,7 @@ const TEST_MODULES = [
   'tests.test_control_state',
   'tests.test_control_panel',
   'tests.test_ux_responsibility_finalization',
+  'tests.test_repair_signal_wiring',
   'tests.test_conversation_controller',
   'tests.test_preflight_versions',
   'tests.test_irodori_cache',
